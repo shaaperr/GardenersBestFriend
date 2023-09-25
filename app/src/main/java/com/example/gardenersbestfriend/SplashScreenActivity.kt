b0918c2942f1b3ce96b5d35ac6922774152fce1e
bildgaul <bildgaul@ut.utm.edu>
2023-09-25 14:10:13 -0500

    Attempting to push project to repository

---
+++ app/src/main/java/com/example/gardenersbestfriend/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.gardenersbestfriend
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 3000)
+    }
+}
