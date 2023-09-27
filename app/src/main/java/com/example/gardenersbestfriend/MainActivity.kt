9aa785629e361e39f28d3ad87d4efecc251ae18e
Vrushank Mali <vrujmali@ut.utm.edu>
2023-09-26 22:42:04 -0500

    Splash screen fades out after 4 seconds and loads the home screen. Having trouble setting up image button functionality so might end up using regular buttons.

--- app/src/main/java/com/example/gardenersbestfriend/MainActivity.kt
+++ app/src/main/java/com/example/gardenersbestfriend/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.gardenersbestfriend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.gardenersbestfriend.R.id.home_button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
