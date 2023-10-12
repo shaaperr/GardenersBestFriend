c1166fc979515c56949bda679159db9f41bc9686
Shakira Perry <shaaperr@ut.utm.edu>
2023-10-11 23:00:31 -0500

    borders added

--- app/src/main/java/com/example/gardenersbestfriend/AllPlantsActivity.kt
+++ app/src/main/java/com/example/gardenersbestfriend/AllPlantsActivity.kt
@@ -1,16 +1,11 @@
 package com.example.gardenersbestfriend
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class AllPlantsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_all_plants)
     }
-
 }
